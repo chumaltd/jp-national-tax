7fb2d5d3e3fa30d7f419b81cca5f76f884d21a88
Chuma Takahiro <chuma@unicorn.limited>
2021-11-01 10:43:28 +0900

    Fix: separate load modules

--- lib/jp_national_tax/income_tax.rb
+++ lib/jp_national_tax/income_tax.rb
@@ -5,40 +5,49 @@ Dir[File.expand_path("income_tax/income*.rb", __dir__)].each { |f| require_relat
 
 module JpNationalTax
   module IncomeTax
-  MOD = [Kouran2020, Nenmatsu2020]
+  MOD_M = [Kouran2020]
+  MOD_Y = [Nenmatsu2020]
 
     module_function
 
     def calc_kouran(pay_amount, pay_date, partner = false, dependent = 0)
-      responsible_module(pay_date)
+      responsible_mod_monthly(pay_date)
         .send(:monthly_kouran, pay_amount, partner, dependent)
         .to_i
     end
 
     def year_salary_taxable(income, calc_date)
-      salary = responsible_module(calc_date)
+      salary = responsible_mod_y(calc_date)
                  .send(:年調給与額, income)
       responsible_module(calc_date)
         .send(:給与所得控除後の給与等の金額, salary)
     end
 
     def basic_deduction(income, calc_date)
-      responsible_module(calc_date)
+      responsible_mod_y(calc_date)
         .send(:基礎控除額, income)
     end
 
     def year_tax(income_taxable, calc_date)
-      income_tax = responsible_module(calc_date)
+      income_tax = responsible_mod_y(calc_date)
                      .send(:算出所得税額, income_taxable)
       responsible_module(calc_date)
         .send(:年調年税額, income_tax)
     end
 
-    def responsible_module(date = nil)
+    def responsible_mod_monthly(date = nil)
+      responsible_module(date, MOD_M)
+    end
+
+    def responsible_mod_y(date = nil)
+      responsible_module(date, MOD_Y)
+    end
+
+    def responsible_module(date = nil, mod)
       raise UndefinedDateError if date.nil?
 
       date = Date.parse(date) if date.class.name == 'String'
-      rules = MOD.map { |mod| [mod.send(:effective_date), mod] }.filter { |a| date >= a[0] }
+      rules = mod.map { |mod| [mod.send(:effective_date), mod] }.filter { |a| date >= a[0] }
       raise NoValidModuleError if rules.length <= 0
 
       rules.sort_by { |a| a[0] }.reverse!.first[1]
